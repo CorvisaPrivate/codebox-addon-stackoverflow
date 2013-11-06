f38fb7ac667f20a5f1c962b4871e294b72526f45
Samy Pessé <samypesse@gmail.com>
2013-11-06 23:25:58 +0100

    Adapt to new codebox

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Stackoverflow (CodeBox add-on)
+Stackoverflow for Codebox
 ========================
 
 Search for Stackoverflow questions directly from your Codebox (https://github.com/FriendCode/codebox).
--- client.js
+++ client.js
@@ -4,27 +4,20 @@ define([], function() {
     var box = require("core/box");
     var search = require("core/search");
 
-    // Execute the search using http proxy from codebox
-    var searchQuestions = function(q, callback) {
-        $.get(box.proxyUrl("https://api.stackexchange.com/2.1/search?order=desc&sort=votes&intitle="+q+"&site=stackoverflow"), callback);
-    };
-
     //Add codebox search handler
     search.handler({
         'id': "stackoverflow",
         'title': "Stackoverflow"
-    }, function(query) {
-        var d = new hr.Deferred();
-        searchQuestions(query, function(data) {
-            d.resolve(_.map(data.items, _.bind(function(result) {
+    }, function(q) {
+        return hr.Requests.getJSON("https://api.stackexchange.com/2.1/search?order=desc&sort=votes&intitle="+q+"&site=stackoverflow&callback=?").then(function(data) {
+            return _.map(data.items, function(result) {
                 return {
                     "text": result.title,
                     "callback": _.bind(function() {
                         window.open(result.link);
                     }, this)
                 };
-            })));
+            });
         });
-        return d;
     });
 });
